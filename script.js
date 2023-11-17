83ff1707d06c4d15c381cbde7da74b6a36dddf78
praveenraj199201 <111525665+praveenraj199201@users.noreply.github.com>
2023-11-17 21:18:26 +0530

    task10

---
+++ script.js
@@ -0,0 +1,33 @@
+setTimeout(()=>{
+    document.getElementById("demo").innerHTML = 10 ;
+    setTimeout(()=>{
+    document.getElementById("demo").innerHTML = 9 ;
+    setTimeout(()=>{
+    document.getElementById("demo").innerHTML = 8 ;
+    setTimeout(()=>{
+    document.getElementById("demo").innerHTML = 7 ;
+    setTimeout(()=>{
+    document.getElementById("demo").innerHTML = 6 ;
+    setTimeout(()=>{
+    document.getElementById("demo").innerHTML = 5 ;
+    setTimeout(()=>{
+    document.getElementById("demo").innerHTML = 4 ;
+    setTimeout(()=>{
+    document.getElementById("demo").innerHTML = 3 ;
+    setTimeout(()=>{
+    document.getElementById("demo").innerHTML = 2 ;
+    setTimeout(()=>{
+    document.getElementById("demo").innerHTML = 1 ;
+    setTimeout(()=>{
+    document.getElementById("demo").innerHTML = "Happy Independence Day!";   
+    },1000)
+    },1000);
+    },1000);
+    },1000);
+    },1000);
+    },1000);
+    },1000);
+    },1000);
+    },1000);
+    },1000);
+    },1000);
